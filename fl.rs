030d381b9d5f9ec7a43449b9ef8dec2521a510f4
s4mple <s4mple@kali.s4mple>
2026-07-29 17:05:51 +0300

    float

--- fl.rs
+++ fl.rs
@@ -4,8 +4,6 @@ fn main() {
   let mut s = String::new();
   io::stdin().read_line(&mut s).expect("err");
   let c: f64 = s.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let d: usize = s2.trim().parse().expect("err");
-  println!("{0:.1$}", c, d);
+  let d = c as i32;
+  println!("{0}\n{1:.3}", d, c - d as f64);
 }
