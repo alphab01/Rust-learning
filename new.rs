42f68a78f134b7df13814e824695f3bdfadf96eb
s4mple <s4mple@kali.s4mple>
2026-07-29 14:26:28 +0300

    new

--- new.rs
+++ new.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    
+  let mut s = String::new();
+  let mut b = String::new();
+  io::stdin().read_line(&mut s).expect("err");
+  io::stdin().read_line(&mut b).expect("err");
+  let a: i32 = b.trim().parse().expect("err");
+  let c: i32 = s.trim().parse().expect("err");
+  let d = c % a;
+  println!("На поле доступно еще {} кв.м свободного места", d);
+  
 }
