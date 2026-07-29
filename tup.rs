540bc9232c9008fe3fde0debe0fbe82847a1d770
s4mple <s4mple@kali.s4mple>
2026-07-30 00:21:04 +0300

    tup task

--- tup.rs
+++ tup.rs
@@ -11,6 +11,11 @@ fn main() {
   io::stdin().read_line(&mut s3).expect("err");
   io::stdin().read_line(&mut s4).expect("err");
   io::stdin().read_line(&mut s5).expect("err");
-  let tup = (s1, s2, s3, s4, s5);
-  println!("{:?}", tup);
+  let a1: f64 = s1.trim().parse().expect("err");
+  let a2: f64 = s2.trim().parse().expect("err");
+  let a3: f64 = s3.trim().parse().expect("err");
+  let a4: f64 = s4.trim().parse().expect("err");
+  let a5: f64 = s5.trim().parse().expect("err");
+  let tup = (a1, a2, a3, a4, a5);
+  println!("{0}, {1}, {2}, {3}, {4}, 0", tup.0 as i64, tup.1 as i64, tup.2 as i64, tup.3 as i64, tup.4 as i64);
 }
