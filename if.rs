454fd1e50bad5b96083c60ccc33a53ea0c44bafd
s4mple <s4mple@kali.s4mple>
2026-07-30 23:51:25 +0300

    interesting thinh

--- if.rs
+++ if.rs
@@ -1,20 +1,17 @@
 use std::io;
 
 fn main() {
+  let arr = [-2.5, 4.2, 9.1, 22.5, 30.0, 1445.123, 1000000.0, 0.001, 0.5, -0.127];
   let mut s = String::new();
   io::stdin().read_line(&mut s).expect("err");
   let c: i64 = s.trim().parse().expect("err");
-  let mut s1 = String::new();
-  io::stdin().read_line(&mut s1).expect("err");
-  let c1: i64 = s1.trim().parse().expect("err");
-  if (c%2 == 0) {
-    println!("Число {} является четным", c);
+  if c < 0 {
+    println!("Отрицательный индекс приводит к панике");
   } else {
-    println!("Число {} является нечетным", c);
-  }
-  if (c1%2 == 0) {
-    println!("Число {} является четным", c1);
-  } else {
-    println!("Число {} является нечетным", c1);
+    if (c > 9) {
+      println!("Попытка выхода за пределы массива");
+    } else {
+      println!("Элемент с индексом {0} равен {1:.3}", c, arr[c as usize]);
+    }
   }
 }
