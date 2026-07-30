d72c71596a818ffcda436e581d03e6fb6ee24bc8
s4mple <s4mple@kali.s4mple>
2026-07-30 16:58:20 +0300

    commit

--- main2.rs
+++ main2.rs
@@ -1,13 +1,8 @@
 use std::io;
 
 fn main() {
-  let mut arr = [-621.5, 11.1, 2.0, -7.123, 0.125, 0.0, 0.000051789];
-  let mut s1 = String::new();
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s1).expect("err");
-  io::stdin().read_line(&mut s2).expect("err");
-  let c1: usize = s1.trim().parse().expect("err");
-  let c2: usize = s2.trim().parse().expect("err");
-  (arr[c1], arr[c2]) = (arr[c2], arr[c1]);
-  println!("{:.9?}", arr);
+  let mut s = String::new();
+  io::stdin().read_line(&mut s).expect("err");
+  let c: u64 = s.trim().parse().expect("err");
+  println!("{0} сек = {1} час {2} минут {3} секунд", c, c / 3600, (c % 3600) / 60, ((c % 3600) % 60));
 }
