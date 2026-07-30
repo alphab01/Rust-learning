e9b1e2adab7a39b8adc9395adb13fc73836e1f6e
s4mple <s4mple@kali.s4mple>
2026-07-30 22:21:59 +0300

    part 2 end

--- pt2.rs
+++ pt2.rs
@@ -4,11 +4,15 @@ fn main() {
   let mut s1 = String::new();
   let mut s2 = String::new();
   let mut s3 = String::new();
+  let mut s4 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
   io::stdin().read_line(&mut s2).expect("err");
   io::stdin().read_line(&mut s3).expect("err");
+  io::stdin().read_line(&mut s4).expect("err");
   let c1: f64 = s1.trim().parse().expect("err");
   let c2: f64 = s2.trim().parse().expect("err");
   let c3: f64 = s3.trim().parse().expect("err");
-  println!("Концентрация получившегося раствора: {:.3}%", (c1*c2) / (c1 + c3));
+  let c4: f64 = s4.trim().parse().expect("err");
+  let d: f64 = (2.0 * c4 - 2.0 * c1 * c3) / ((c3 - 1.0) * c3);
+  println!("За {0} день турист прошел {1:.3} км", c2, c1 + (c2 - 1.0) * d);
 }
