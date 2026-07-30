f4c24c35d0c19aba422922023e69dae5fe9d930a
s4mple <s4mple@kali.s4mple>
2026-07-31 00:11:18 +0300

    to samoye zadanye Combinatoryca

--- if.rs
+++ if.rs
@@ -3,10 +3,29 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let c1: i64 = s1.trim().parse().expect("err");
-  if ((c1 % 4 == 0 && c1 % 100 != 0) || (c1%400 == 0)) {
-    println!("{} является високосным годом", c1);
-  } else {
-    println!("{} не является високосным годом", c1);
+  let mut c1: f64 = s1.trim().parse().expect("err");
+  let mut s2 = String::new();
+  io::stdin().read_line(&mut s2).expect("err");
+  let mut c2: f64 = s2.trim().parse().expect("err");
+  let mut s3 = String::new();
+  io::stdin().read_line(&mut s3).expect("err");
+  let mut c3: f64 = s3.trim().parse().expect("err");
+  if c1 > c2 && c1 > c3 && c2 > c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c3, c2, c1);
+  }
+  if c1 > c2 && c1 < c3 && c2 < c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c2, c1, c3);
+  }
+  if c1 > c2 && c1 > c3 && c2 < c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c2, c3, c1);
+  }
+  if c1 < c2 && c1 > c3 && c2 > c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c3, c1, c2);
+  }
+  if c1 < c2 && c1 < c3 && c2 > c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c1, c3, c2);
+  }
+  if c1 < c2 && c1 < c3 && c2 < c3 {
+    println!("{0:.1}, {1:.1}, {2:.1}", c1, c2, c3);
   }
 }
