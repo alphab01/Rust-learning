820a9fea35bf3dfa0c608e236abb316a5abd3b9a
s4mple <s4mple@kali.s4mple>
2026-07-30 23:46:05 +0300

    endif

--- if.rs
+++ if.rs
@@ -3,10 +3,18 @@ use std::io;
 fn main() {
   let mut s = String::new();
   io::stdin().read_line(&mut s).expect("err");
-  let c: f64 = s.trim().parse().expect("err");
-  if (c > 0.0) {
-    println!("Число {:.1} является положительным", c);
+  let c: i64 = s.trim().parse().expect("err");
+  let mut s1 = String::new();
+  io::stdin().read_line(&mut s1).expect("err");
+  let c1: i64 = s1.trim().parse().expect("err");
+  if (c%2 == 0) {
+    println!("Число {} является четным", c);
   } else {
-    println!("Число {:.1} является отрицательным", c);
+    println!("Число {} является нечетным", c);
+  }
+  if (c1%2 == 0) {
+    println!("Число {} является четным", c1);
+  } else {
+    println!("Число {} является нечетным", c1);
   }
 }
