ff31acb3500016e930497068057c31507f9d9127
s4mple <s4mple@kali.s4mple>
2026-07-31 12:01:06 +0300

    comb

--- if.rs
+++ if.rs
@@ -3,29 +3,26 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let mut c1: f64 = s1.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let mut c2: f64 = s2.trim().parse().expect("err");
-  let mut s3 = String::new();
-  io::stdin().read_line(&mut s3).expect("err");
-  let mut c3: f64 = s3.trim().parse().expect("err");
-  if c1 > c2 && c1 > c3 && c2 > c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c3, c2, c1);
+  let mut c: i64 = s1.trim().parse().expect("err");
+  let mut ma: i64 = 0;
+  let (mut c1, mut c2, mut c3) = (c/100, (c/10)%10, c%10);
+  if (c1 * 100 + c2 * 10 + c3 > ma) {
+    ma = c1 * 100 + c2 * 10 + c3;
   }
-  if c1 > c2 && c1 < c3 && c2 < c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c2, c1, c3);
+  if (c2 * 100 + c1 * 10 + c3 > ma) {
+    ma = c2 * 100 + c1 * 10 + c3;
   }
-  if c1 > c2 && c1 > c3 && c2 < c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c2, c3, c1);
+  if (c3 * 100 + c2 * 10 + c1 > ma) {
+    ma = c3 * 100 + c2 * 10 + c1;
   }
-  if c1 < c2 && c1 > c3 && c2 > c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c3, c1, c2);
+  if (c1 * 100 + c3 * 10 + c2 > ma) {
+    ma = c1 * 100 + c3 * 10 + c2;
   }
-  if c1 < c2 && c1 < c3 && c2 > c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c1, c3, c2);
+  if (c3 * 100 + c1 * 10 + c2 > ma) {
+    ma = c3 * 100 + c1 * 10 + c2;
   }
-  if c1 < c2 && c1 < c3 && c2 < c3 {
-    println!("{0:.1}, {1:.1}, {2:.1}", c1, c2, c3);
+  if (c2 * 100 + c3 * 10 + c1 > ma) {
+    ma = c2 * 100 + c3 * 10 + c1;
   }
+  println!("{ma}");
 }
