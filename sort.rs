6033dab8be83aa91586110b0187fb69978a2434e
s4mple <s4mple@kali.s4mple>
2026-07-31 23:53:39 +0300

    end if elif let if

---
+++ sort.rs
@@ -0,0 +1,84 @@
+fn main() {
+  let mut array = [5, -2, 8, -1, 3];
+  if (array[0] > array[1]) {
+    let mut c: i64 = array[0];
+    array[0] = array[1];
+    array[1] = c;
+  }
+  if (array[1] > array[2]) {
+    let mut c: i64 = array[1];
+    array[1] = array[2];
+    array[2] = c;
+  }
+  if (array[2] > array[3]) {
+    let mut c: i64 = array[2];
+    array[2] = array[3];
+    array[3] = c;
+  }
+  if (array[3] > array[4]) {
+    let mut c: i64 = array[3];
+    array[3] = array[4];
+    array[4] = c;
+  }
+  if (array[0] > array[1]) {
+    let mut c: i64 = array[0];
+    array[0] = array[1];
+    array[1] = c;
+  }
+  if (array[1] > array[2]) {
+    let mut c: i64 = array[1];
+    array[1] = array[2];
+    array[2] = c;
+  }
+  if (array[2] > array[3]) {
+    let mut c: i64 = array[2];
+    array[2] = array[3];
+    array[3] = c;
+  }
+  if (array[3] > array[4]) {
+    let mut c: i64 = array[3];
+    array[3] = array[4];
+    array[4] = c;
+  }
+  if (array[0] > array[1]) {
+    let mut c: i64 = array[0];
+    array[0] = array[1];
+    array[1] = c;
+  }
+  if (array[1] > array[2]) {
+    let mut c: i64 = array[1];
+    array[1] = array[2];
+    array[2] = c;
+  }
+  if (array[2] > array[3]) {
+    let mut c: i64 = array[2];
+    array[2] = array[3];
+    array[3] = c;
+  }
+  if (array[3] > array[4]) {
+    let mut c: i64 = array[3];
+    array[3] = array[4];
+    array[4] = c;
+  }
+  if (array[0] > array[1]) {
+    let mut c: i64 = array[0];
+    array[0] = array[1];
+    array[1] = c;
+  }
+  if (array[1] > array[2]) {
+    let mut c: i64 = array[1];
+    array[1] = array[2];
+    array[2] = c;
+  }
+  if (array[2] > array[3]) {
+    let mut c: i64 = array[2];
+    array[2] = array[3];
+    array[3] = c;
+  }
+  if (array[3] > array[4]) {
+    let mut c: i64 = array[3];
+    array[3] = array[4];
+    array[4] = c;
+  }
+  println!("{:?}", array);
+}
