bf53fdfd210b52c53ab49e640e9df3d114853716
s4mple <s4mple@kali.s4mple>
2026-08-01 13:05:40 +0300

    final 9

--- m8.rs
+++ m8.rs
@@ -4,21 +4,19 @@ fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
   let mut c1: i64 = s1.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let mut c2: i64 = s2.trim().parse().expect("err");
-  let mut s3 = String::new();
-  io::stdin().read_line(&mut s3).expect("err");
-  let mut c3: i64 = s3.trim().parse().expect("err");
-  if (c1 == c2 && c2 == c3) {
-    println!("Числа {0}, {1} и {2} образуют равносторонний треугольник", c1, c2, c3);
-  } else if ((c1 == c2 && c2 != c3) || (c1 == c3 && c2 != c3) || (c2 == c3 && c1 != c3)) {
-    println!("Числа {0}, {1} и {2} образуют равнобедренный треугольник", c1, c2, c3);
+  if (c1 == 1 || c1 == 2 || c1 == 4) {
+    println!("Для {0}! последняя цифра равна {0}", c1);
+  } else if (c1 == 3) {
+    println!("Для 3! последняя цифра равна 6");
   } else {
-    if ((c1 + c2 > c3) && (c1 + c3 > c2) && (c2 + c3 > c1)) {
-      println!("Числа {0}, {1} и {2} образуют разносторонний треугольник", c1, c2, c3);
-    } else {
-      println!("Числа {c1}, {c2} и {c3} не образуют треугольник");
-    }
+    println!("Для {}! последняя цифра равна 0", c1);
   }
 }
+/*
+1 1
+2 2
+3 6
+4 4
+5 0
+
+*/
