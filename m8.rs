de75b34080332b70f3f89f0e566526e18f8a16ee
s4mple <s4mple@kali.s4mple>
2026-08-01 10:38:17 +0300

    date

--- m8.rs
+++ m8.rs
@@ -1,35 +1,70 @@
 use std::io;
 
 fn main() {
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
   let mut c1: u8 = s1.trim().parse().expect("err");
-  if (s2.trim() == "Север".to_string()) {
-    if (c1 == 1) {
-      s2 = "Запад".to_string();
-    } else if (c1 == 2) {
-      s2 = "Восток".to_string();
+  let mut s2 = String::new();
+  io::stdin().read_line(&mut s2).expect("err");
+  let mut c2: u8 = s2.trim().parse().expect("err");
+  let mut s3 = String::new();
+  io::stdin().read_line(&mut s3).expect("err");
+  let mut c3: u16 = s3.trim().parse().expect("err");
+  if ((c3 % 4 == 0 && c3 % 100 != 0) || c3 % 400 == 0) {
+    if (c2 == 1 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 2 && c1 > 0 && c1 < 30) {
+      println!("Дата корректна!");
+    } else if (c2 == 3 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 4 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 5 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 6 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 7 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 8 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 9 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 10 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 11 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 12 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else {
+      println!("Дата некорректна!");
     }
-  } else if (s2.trim() == "Запад".to_string()) {
-    if (c1 == 1) {
-      s2 = "Юг".to_string();
-    } else if (c1 == 2) {
-      s2 = "Север".to_string();
-    }
-  } else if (s2.trim() == "Юг".to_string()) {
-    if (c1 == 1) {
-      s2 = "Восток".to_string();
-    } else if (c1 == 2) {
-      s2 = "Запад".to_string();
-    }
-  } else if (s2.trim() == "Восток".to_string()) {
-    if (c1 == 1) {
-      s2 = "Север".to_string();
-    } else if (c1 == 2) {
-      s2 = "Юг".to_string();
+  } else {
+    if (c2 == 1 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 2 && c1 > 0 && c1 < 29) {
+      println!("Дата корректна!");
+    } else if (c2 == 3 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 4 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 5 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 6 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 7 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 8 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 9 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 10 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else if (c2 == 11 && c1 > 0 && c1 < 31) {
+      println!("Дата корректна!");
+    } else if (c2 == 12 && c1 > 0 && c1 < 32) {
+      println!("Дата корректна!");
+    } else {
+      println!("Дата некорректна!");
     }
   }
-  println!("Направление лунохода после выполнения команды: {}", s2);
 }
