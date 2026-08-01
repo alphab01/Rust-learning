c76acfc5e35771797c68b6fd1bb097bf82cb16c1
s4mple <s4mple@kali.s4mple>
2026-08-01 20:38:10 +0300

    movik

--- mask.rs
+++ mask.rs
@@ -3,9 +3,9 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let c1: i8 = s1.trim().parse().expect("err");
+  let c1: u8 = s1.trim().parse().expect("err");
   let mut s2 = String::new();
   io::stdin().read_line(&mut s2).expect("err");
   let c2: u8 = s2.trim().parse().expect("err");
-  println!("x до очистки {0}-го бита\nв двоичной записи: {1:08b}\nв десятичной записи: {1}\n\nx после очистки {0}-го бита\nв двоичной записи: {2:08b}\nв десятичной записи: {2}", c2, c1, c1 & (!(1 << c2)));
+  println!("{0}-й бит числа {1:08b} равен {2}", c2, c1, if (c1 & (1 << c2) > 0) {1} else {0});
 }
