98a134e17cd748810a9ed7665b383585204cf0f5
s4mple <s4mple@kali.s4mple>
2026-08-02 21:10:50 +0300

    XDDDD

--- nums.rs
+++ nums.rs
@@ -3,19 +3,10 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let mut c1: i64 = s1.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let mut c2: u8 = s2.trim().parse().expect("err");
-  let mut c: i64 = 0;
-  for i in 0..c2 {
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).expect("err");
-    let xD: i64 = s.trim().parse().expect("err");
-    if (xD > c1) {
-      c += 1;
-    }
+  let mut c1: u32 = s1.trim().parse().expect("err");
+  let xd: usize = c1 as usize;
+  for i in 0..2_u32.pow(c1) {
+    print!("{:0xd$b} | {1}\n", i, if (i != 2_u32.pow(c1) - 1) {0} else {1});
   }
-  println!("Количество элементов больших {c1}: {c}");
 }
 
