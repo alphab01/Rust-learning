e28246d1a4a79b7a5aba1de5d0bbdee233667506
s4mple <s4mple@kali.s4mple>
2026-08-02 03:29:27 +0300

    cycle pt2

--- cycle.rs
+++ cycle.rs
@@ -5,11 +5,8 @@ fn main() {
   io::stdin().read_line(&mut s2).expect("err");
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let c1: i32 = s1.trim().parse().expect("err");
-  let mut s3 = String::new();
-  io::stdin().read_line(&mut s3).expect("err");
-  let c3: i32 = s3.trim().parse().expect("err");
-  for i in c1..c3{
-    println!("{}", s2.trim());
+  let c1: u16 = s1.trim().parse().expect("err");
+  for i in 0..c1 {
+    println!("{0}: {1}", i, s2.trim());
   }
 }
