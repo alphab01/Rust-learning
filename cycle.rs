afeda4ecf4b823ca296f4952a00c39982a7165f8
s4mple <s4mple@kali.s4mple>
2026-08-02 16:14:07 +0300

    minmax normal

--- cycle.rs
+++ cycle.rs
@@ -3,11 +3,22 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let c1: i32 = s1.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let c2: i32 = s2.trim().parse().expect("err");
-  for i in (c1..=c2).rev() {
-    println!("{i}");
+  let c1: u8 = s1.trim().parse().expect("err");
+  let (mut ma, mut mi) = (0, 0);
+  for i in 0..c1 {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("err");
+    let c: i32 = s.trim().parse().expect("err");
+    if (i == 0) {
+      (ma, mi) = (c, c);
+    } else {
+      if (ma < c) {
+        ma = c;
+      }
+      if (mi > c) {
+        mi = c;
+      }
+    }
   }
+  println!("Максимальное число: {0}\nМинимальное число: {1}", ma, mi);
 }
