e752fadfeedf47c7fdc2dfc4b8fa30de67565456
s4mple <s4mple@kali.s4mple>
2026-08-02 03:33:47 +0300

    comeback

--- cycle.rs
+++ cycle.rs
@@ -1,12 +1,21 @@
 use std::io;
 
 fn main() {
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let c1: u16 = s1.trim().parse().expect("err");
-  for i in 0..c1 {
-    println!("{0}: {1}", i, s2.trim());
+  let c1: i16 = s1.trim().parse().expect("err");
+  let mut s2 = String::new();
+  io::stdin().read_line(&mut s2).expect("err");
+  let c2: i16 = s2.trim().parse().expect("err");
+  for i in 1..=10 {
+    println!("{0} x {1} = {2}", c1, i, c1 * i);
   }
+  println!("");
+  for i in 1..=10 {
+    println!("{0} x {1} = {2}", c2, i, c2 * i);
+  } 
 }
+
+/*
+Если кто-то когда-то увидит этот код и время его отправки, то я надеюсь вас не смутит актуальность
+*/
