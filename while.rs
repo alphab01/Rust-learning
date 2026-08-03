4aac161e257ca9ceb97ff014db83b516df4ba998
s4mple <s4mple@kali.s4mple>
2026-08-03 13:37:26 +0300

    it works 0_0

--- while.rs
+++ while.rs
@@ -3,23 +3,12 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let mut c1: i64 = s1.trim().parse().expect("err");
-  print!("Число {} состоит из цифр: ", c1);
-  let mut c: i64 = 0;
-  let mut c2: i64 = c1;
+  let mut c1: u8 = s1.trim().parse().expect("err");
+  let mut c: u8 = 0;
+  let s: u8 = c1;
   while (c1 != 0) {
-    c1 /= 10;
-    c += 1;
+    c += c1 % 2;
+    c1 /= 2;
   }
-  let mut i: i64 = 1;
-  while (i <= c) {
-    if (i != c) {
-      print!("{} ", (c2 / (10_u32.pow((c - i) as u32)) as i64)%10 as i64);
-      i += 1;
-    } else {
-      print!("{}\n", (c2 / (10_u32.pow((c - i) as u32)) as i64)%10 as i64);
-      i += 1;
-    }
-  }
-  println!("Число {0} является {1} значным", c2, c);
+  println!("Количество установленных битов в {0:08b} равно {1}", s, c);
 }
