8173a5e2ad0ee675f85770d6aba1dce1b08d7f91
s4mple <s4mple@kali.s4mple>
2026-08-03 20:20:53 +0300

    del

--- deleter.rs
+++ deleter.rs
@@ -3,19 +3,16 @@ use std::io;
 fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
-  let mut c1: u32 = s1.trim().parse().expect("err");
+  let mut c1: i64 = s1.trim().parse().expect("err");
   let mut s2 = String::new();
   io::stdin().read_line(&mut s2).expect("err");
-  let mut c2: u32 = s2.trim().parse().expect("err");
-  let mut c: u32 = c1;
-  let mut i: u32 = 0;
-  while (c > 0) {
-    i += 1;
-    c /= 10;
-  }
-  if (i <= c2) {
-    println!("k >= n");
-  } else {
-    println!("{}", c1 % (10_u32.pow(i - c2)));
+  let mut c2: i64 = s2.trim().parse().expect("err");
+  for i in 0..c2 {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("err");
+    let mut c: i64 = s.trim().parse().expect("err");
+    if (c % c1 == 0) {
+      println!("{c}");
+    }
   }
 }
