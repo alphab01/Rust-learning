f0a74fe48ddaf04d775068b5dd1651d980edebca
s4mple <s4mple@kali.s4mple>
2026-08-03 20:39:39 +0300

    reverse

---
+++ vloj.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+  let mut arr: [i64; 10] = [0; 10];
+  for i in 0..10 {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("err");
+    let mut c: i64 = s.trim().parse().expect("err");
+    arr[9 - i] = c;
+  }
+  println!("{:?}", arr);
+}
