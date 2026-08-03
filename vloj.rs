d2f76e197b8fa0ac10b5971db7f318982a506ea0
s4mple <s4mple@kali.s4mple>
2026-08-03 23:40:17 +0300

    big posled

--- vloj.rs
+++ vloj.rs
@@ -1,24 +1,35 @@
 use std::io;
 
 fn main() {
-  let mut s = String::new();
-  io::stdin().read_line(&mut s).expect("err");
-  let mut c: u32 = s.trim().parse().expect("err");
-  let mut s1 = String::new();
-  io::stdin().read_line(&mut s1).expect("err");
-  let mut c1: u32 = s1.trim().parse().expect("err");
-  let mut i: u32 = 1;
-  while i <= c {
-    for j in 0..c1 {
-      if (j != c1 - 1 && i != c) {
-        print!("{i} ");
-      } else {
-        print!("{i}\n");
-      }
-      i += 1;
-      if (i > c) {
+  let mut arr: [i64; 10] = [0; 10];
+  for i in 0..10 {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("err");
+    arr[i] = s.trim().parse().expect("err");
+  }
+  let mut i1: i64 = 0;
+  let mut ma: i64 = 0;
+  for i in 0..10 {
+    let mut s: i64 = 1;
+    let mut c = arr[i];
+    for j in (i + 1)..10 {
+      if (arr[j] <= c) {
         break;
+      } else {
+        s += 1;
+        c = arr[j];
       }
+    }
+    if (s > ma) {
+      ma = s;
+      i1 = i as i64;
+    }
+  }
+  for i in i1..(i1 + ma) {
+    if (i != (i1 + ma - 1)) {
+      print!("{} ", arr[i as usize]);
+    } else {
+      print!("{}\n", arr[i as usize]);
     }
   }
 }
