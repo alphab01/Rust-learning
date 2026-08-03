d2344230006c45e7d144c5c4a269e476de4018a0
s4mple <s4mple@kali.s4mple>
2026-08-03 20:48:42 +0300

    tree

--- vloj.rs
+++ vloj.rs
@@ -1,21 +1,16 @@
 use std::io;
 
 fn main() {
-  let mut arr: [i64; 10] = [0; 10];
-  for i in 0..10 {
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).expect("err");
-    let mut c: i64 = s.trim().parse().expect("err");
-    arr[9 - i] = c;
-  }
-  for i in 0..10 {
-    for j in 0..9 {
-      if (arr[j] > arr[j + 1]) {
-        arr[j] ^= arr[j + 1];
-        arr[j + 1] ^= arr[j];
-        arr[j] ^= arr[j + 1];
-      }
+  let mut s = String::new();
+  io::stdin().read_line(&mut s).expect("err");
+  let mut c: u8 = s.trim().parse().expect("err");
+  for i in 0..c {
+    for j in 0..(c - i - 1) {
+      print!(" ");
     }
+    for j in 0..(1 + i * 2) {
+      print!("*");
+    }
+    println!("");
   }
-  println!("{:?}", arr);
 }
