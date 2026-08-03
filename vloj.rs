5c1587059236a37960499b178c4d98298abd4090
s4mple <s4mple@kali.s4mple>
2026-08-03 20:42:53 +0300

    bubble

--- vloj.rs
+++ vloj.rs
@@ -8,5 +8,14 @@ fn main() {
     let mut c: i64 = s.trim().parse().expect("err");
     arr[9 - i] = c;
   }
+  for i in 0..10 {
+    for j in 0..9 {
+      if (arr[j] > arr[j + 1]) {
+        arr[j] ^= arr[j + 1];
+        arr[j + 1] ^= arr[j];
+        arr[j] ^= arr[j + 1];
+      }
+    }
+  }
   println!("{:?}", arr);
 }
