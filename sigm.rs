0bfa552625a7bec3183c8555d579878d43dbfd28
s4mple <s4mple@kali.s4mple>
2026-08-04 13:30:02 +0300

    sigm

--- sigm.rs
+++ sigm.rs
@@ -4,12 +4,14 @@ fn main() {
   let mut s1 = String::new();
   io::stdin().read_line(&mut s1).expect("err");
   let mut c1: i64 = s1.trim().parse().expect("err");
-  let mut s2 = String::new();
-  io::stdin().read_line(&mut s2).expect("err");
-  let mut c2: f64 = s2.trim().parse().expect("err");
-  let mut s: f64 = 1.0;
-  for i in 0..=c1 {
-    s *= (c2 + 1.0)/(((c2 * c2)/2.0) + 4.0);
+  let mut s: f64 = 0.0;
+  for i in 1..=c1 {
+    if (i%2 != 0) {
+      s += 1.0/(i as f64);
+    } else {
+      s -= 1.0/(i as f64);
+    }
   }
-  println!("{:.5}", s);
+  let mut c: usize = c1 as usize;
+  println!("{:.c$}", s);
 }
